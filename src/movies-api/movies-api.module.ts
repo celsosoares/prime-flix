0583d114377212380206536d0f27e01ab4ead165
celsosoares <celso.soares97c@gmail.com>
2023-09-28 21:09:38 -0300

    Adds getTopMovies method
    - Adds env.template file
    - Adjustments in movies-api module

--- src/movies-api/movies-api.module.ts
+++ src/movies-api/movies-api.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MoviesApiService } from './service/movies-api.service';
 import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from 'nestjs-config';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, ConfigModule],
   providers: [MoviesApiService],
   exports: [MoviesApiService],
 })
--- src/movies-api/service/movies-api.service.ts
+++ src/movies-api/service/movies-api.service.ts
@@ -1,51 +1,39 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable, Logger } from '@nestjs/common';
-import { AxiosError } from 'axios';
-import { catchError, firstValueFrom } from 'rxjs';
+import { Injectable } from '@nestjs/common';
+import { lastValueFrom } from 'rxjs';
 import { MoviesApiDto } from '../dto/movies-api.dto';
+import { plainToInstance } from 'class-transformer';
 
 @Injectable()
 export class MoviesApiService {
-  private readonly logger = new Logger(MoviesApiService.name);
+  private readonly THEMOVIEDB_API_URL = process.env.THEMOVIEDB_API_URL;
+  private readonly THEMOVIEDB_API_KEY = process.env.THEMOVIEDB_API_KEY;
+
   constructor(private http: HttpService) {}
 
   async getPopularMovies(): Promise<MoviesApiDto> {
-    const url = 'https://api.themoviedb.org/3/movie/popular';
-    const { data } = await firstValueFrom(
-      this.http
-        .get<MoviesApiDto>(url, {
-          withCredentials: true,
-          params: {
-            api_key: '047f6c34ed9a0403faf9fcd9e4e89489',
-          },
-        })
-        .pipe(
-          catchError((error: AxiosError) => {
-            this.logger.error(error.response.data);
-            throw 'An error happened!';
-          }),
-        ),
+    const url = `${this.THEMOVIEDB_API_URL}/movie/popular`;
+    const config = {
+      headers: { Authorization: `Bearer ${this.THEMOVIEDB_API_KEY}` },
+    };
+
+    const response = await lastValueFrom(
+      this.http.get<MoviesApiDto>(url, config),
     );
-    return data;
+
+    return plainToInstance(MoviesApiDto, response?.data);
   }
 
   async getTopRatedMovies(): Promise<MoviesApiDto> {
-    const url = 'https://api.themoviedb.org/3/movie/top_rated';
-    const { data } = await firstValueFrom(
-      this.http
-        .get<MoviesApiDto>(url, {
-          withCredentials: true,
-          params: {
-            api_key: '047f6c34ed9a0403faf9fcd9e4e89489',
-          },
-        })
-        .pipe(
-          catchError((error: AxiosError) => {
-            this.logger.error(error.response.data);
-            throw 'An error happened!';
-          }),
-        ),
+    const url = `${this.THEMOVIEDB_API_URL}/movie/top_rated`;
+    const config = {
+      headers: { Authorization: `Bearer ${this.THEMOVIEDB_API_KEY}` },
+    };
+
+    const response = await lastValueFrom(
+      this.http.get<MoviesApiDto>(url, config),
     );
-    return data;
+
+    return plainToInstance(MoviesApiDto, response?.data);
   }
 }
